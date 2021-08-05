11128ae8cac261d20890ecfd49f14a9a9d281700
devjjongs <whdtjr5689@gmail.com>
2021-08-05 16:09:32 +0900

    add & modified files

--- DebugTest/src/main/java/com/bit/spring/LoginPrcessController.java
+++ DebugTest/src/main/java/com/bit/spring/LoginPrcessController.java
@@ -18,122 +18,124 @@ import com.bit.spring.model.template.dao.MemberDao;
 
 @Controller
 public class LoginPrcessController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(LoginPrcessController.class);
-	
-	
+
 	/*
 	 * ApplicationContext context = new
 	 * ClassPathXmlApplicationContext("annoTestContext.xml");
 	 */
-	
-	MemberDAO dao = new MemberDAO();//jdbc
+
+	MemberDAO dao = new MemberDAO();// jdbc
 //	dao.connection();
-	
+
 	@Autowired
-	MemberDao memberDao ;  //spring jdbc template
-	
+	MemberDao memberDao; // spring jdbc template
+
 	@Autowired
-	MemberDaoMyBatis memberDaoMyBatis;//orm 
-	
-	
+	MemberDaoMyBatis memberDaoMyBatis;// orm
+
 	@RequestMapping("loginForm")
 	public String loginForm() {
-		
-		// db 선처리부분을 실행할 수 있기 
-		
-		
+
+		// db 선처리부분을 실행할 수 있기
+
 		System.out.println("loginForm() ");
 		return "loginForm";
 	}
+
 	/**
 	 * 호출하는 이름이 쓰여져 있지 않다 그래서, loginCheck로 호출한다
+	 * 
 	 * @param userId
 	 * @param userPw
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping( method = RequestMethod.POST )
-	public String loginCheck(String userId , String userPw , Model model) {
-		//userName , userPw  ==> 디비에 유무 판단 
+	@RequestMapping(method = RequestMethod.POST)
+	public String loginCheck(String userId, String userPw, Model model) {
+		// userName , userPw ==> 디비에 유무 판단
 //		System.out.println(userName + " " +userPw );
 //		logger.info(userName + " " +userPw);
-		
+
 //		MemberDAO dao = new MemberDAO();
 //		dao.connection();
-		
-		Member member =  dao.loginCheck(userId, userPw);
-		if(member==null) {
+
+		Member member = dao.loginCheck(userId, userPw);
+		if (member == null) {
 			return "loginForm";
-		}else {
+		} else {
 			model.addAttribute("member", member);
-			return "home";//home.jsp call
+			return "home";// home.jsp call
 		}
 	}
+
 	/**
 	 * Orm MyBatis에 의해서 처리된 결과를 이용
+	 * 
 	 * @param userId
 	 * @param userPw
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(method = RequestMethod.POST , value = "getMember")
-	public String getMember(@RequestParam("xxx")    String userId , String userPw , Model model) {
-		
+	@RequestMapping(method = RequestMethod.POST, value = "getMember")
+	public String getMember(@RequestParam("xxx") String userId, String userPw, Model model) {
+
 		com.bit.spring.model.orm.vo.Member member = memberDaoMyBatis.getMember(userId, userPw);
-		
-		if(member==null) {
-			//return "loginForm";//  /WEB-INF/views/loginForm.jsp call
-			return "redirect:/loginForm";//  @RequestMapping("loginForm")  call
-		}else {
-			
+
+		if (member == null) {
+			// return "loginForm";// /WEB-INF/views/loginForm.jsp call
+			return "redirect:/loginForm";// @RequestMapping("loginForm") call
+		} else {
+
 			model.addAttribute("member", member);
-			return "home";//   /WEB-INF/views/home.jsp call
+			return "home";// /WEB-INF/views/home.jsp call
 		}
 	}
-	
-	
-	
-	
+
 	@RequestMapping("memberListJdbc")
 	public String memberListJdbc(Model model) {
-		model.addAttribute("msg","memberListJdbc" );
-		
-		model.addAttribute("memberList",  dao.memberList()  );//key="memberList"  , value=List<Member>
-		
-		
-		
-		return "memberList";//memberList.jsp
+		model.addAttribute("msg", "memberListJdbc");
+
+		model.addAttribute("memberList", dao.memberList());// key="memberList" , value=List<Member>
+
+		return "memberList";// memberList.jsp
 	}
-	
+
 	@RequestMapping("memberListTemplate")
 	public String memberListTemplate(Model model) {
-		model.addAttribute("msg","memberListTemplate" );
+		model.addAttribute("msg", "memberListTemplate");
 		model.addAttribute("memberList", memberDao.memberList());
 		return "memberList";
 	}
-	
+
 	@RequestMapping("memberListOrm")
 	public String memberListOrm(Model model) {
-		model.addAttribute("msg","memberListOrm" );
-		model.addAttribute("memberList", memberDaoMyBatis.getAllMember())    ;
+		model.addAttribute("msg", "memberListOrm");
+		model.addAttribute("memberList", memberDaoMyBatis.getAllMember());
 		return "memberList";
 	}
-	
+
 	@RequestMapping("findByColumn")
 	public  String   findByColumn(String column,String value,Model model ) {
+		if (column.trim().length()==0) {
+			column="email";
+		}
+		System.err.println("==>"+column+"<==");
+		System.err.println("==>"+column.trim()+"<==");
 		model.addAttribute("memberList",memberDaoMyBatis.findByColumn(column, value));
 		return "memberList";
 		
 	}
+
 	/**
-	 * loginForm.jsp에서 Member 자료를 입력받아서 orm.dao.MemberDao~~.memberInsert()
-	 * 결과에 따라서 home.jsp ,  입력 controller의 메소드 호출
+	 * loginForm.jsp에서 Member 자료를 입력받아서 orm.dao.MemberDao~~.memberInsert() 결과에 따라서
+	 * home.jsp , 입력 controller의 메소드 호출
 	 */
-	@RequestMapping(value = "memberInsert" , method=RequestMethod.POST)
-	public String memberInsert(com.bit.spring.model.orm.vo.Member member , Model model) {
+	@RequestMapping(value = "memberInsert", method = RequestMethod.POST)
+	public String memberInsert(com.bit.spring.model.orm.vo.Member member, Model model) {
 		logger.info(member.toString());
-		if(member!=null && member.getUserId().length()>=5) {
+		if (member != null && member.getUserId().length() >= 5) {
 			memberDaoMyBatis.memberInsert(member);
 			model.addAttribute("member", member);
 			return "home";
@@ -141,27 +143,3 @@ public class LoginPrcessController {
 		return "redirect:/loginForm";
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
