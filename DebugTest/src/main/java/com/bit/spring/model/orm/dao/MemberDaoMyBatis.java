f072382210cd70bca59a1c831420096cbfcf047d
devjjongs <whdtjr5689@gmail.com>
2021-08-06 17:33:21 +0900

    add & modified code

--- DebugTest/src/main/java/com/bit/spring/model/orm/dao/MemberDaoMyBatis.java
+++ DebugTest/src/main/java/com/bit/spring/model/orm/dao/MemberDaoMyBatis.java
@@ -1,6 +1,7 @@
 package com.bit.spring.model.orm.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -30,6 +31,12 @@ public interface MemberDaoMyBatis {
 	
 	// 회원 정보 insert 메소드 
 	void memberInsert(Member member );
-	
-	void paramMap
+	void paramMapInsert(Map<String, Object> paramMap);
 }
+
+
+
+
+
+
+
